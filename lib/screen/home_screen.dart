cc0c81e72625253ef0bc6ffad1a73a38a3f198c1
kdw <50583568+dong336@users.noreply.github.com>
2024-12-20 22:17:26 +0900

    Initial commit

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final WebViewController _controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onHttpError: (HttpResponseError error) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://naver.com')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+    ..loadRequest(Uri.parse('https://www.youtube.com/'));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('Code Factory'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              _controller.loadRequest(Uri.parse('https://www.youtube.com/'));
+            },
+            icon: Icon(Icons.home),
+          ),
+        ],
+      ),
+      body: WebViewWidget(
+        controller: _controller,
+      ),
+    );
+  }
+}
